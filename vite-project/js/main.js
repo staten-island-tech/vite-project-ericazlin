18743a33a479cf0c942755c993ceb4c7da807190
ericazlin <ericazlin@gmail.com>
2022-12-05 11:46:05 -0500

    why no put in div

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,10 +2,8 @@ import '../styles/style.css'
 import { setupCounter } from './counter.js'
 
 const DOMSelectors = {
-  cards: document.querySelector("#products"),
-
+  products: document.getElementById("products"),
 }
-
 
 const products = [
   {
@@ -15,19 +13,31 @@ const products = [
   {
     name: "chips",
     image: `https://cdn.discordapp.com/attachments/970059739318849597/1049201473617141872/chips.jpg`
-  }
+  },
+  {
+    name: "minced meat",
+    image: "https://cdn.discordapp.com/attachments/970059739318849597/1049201474149822515/minced_meat.jpg"
+  },
+  {
+    name: "whole bread",
+    image: `https://cdn.discordapp.com/attachments/970059739318849597/1049201474380505128/whole_bread.jpg`
+  },
+  {
+    name: "windscreen washer",
+    image: `https://cdn.discordapp.com/attachments/970059739318849597/1049201474653143050/windscreen_washer.jpg`
+  },
 ]
 
 
-products.forEach(element => {
-  DOMSelectors.cards.insertAdjacentHTML("beforebegin",
+products.forEach(card => {
+  DOMSelectors.products.insertAdjacentHTML("beforebegin",
     `
-  <div id=card>
-  <img src=${element.image})image}>
-  <h1> this is ${element.name} </h1>
-  </img>
-  </div>
-  `
+    <div id=card>
+    <img src=${card.image}>
+    <h1> this is ${card.name} </h1>
+    </img>
+    </div>
+    `
   )
 })
 
