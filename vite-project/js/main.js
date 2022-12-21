811b67788325229387fb4ea7f238dc8ce2a6955c
ericazlin <ericazlin@gmail.com>
2022-12-21 00:43:41 -0500

    made it 3 thingies wide and js for filtering and css for the card color

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -4,38 +4,69 @@ import { products } from './products.js'
 const DOMSelectors = {
   products1: document.getElementById("products1"),
   products2: document.getElementById("products2"),
+  products3: document.getElementById("products3"),
+  everything: document.querySelector(".everything"),
+  everything2: document.querySelector(".everything2"),
   btn: document.querySelector(".btn"),
   filter: document.getElementById("filter"),
+  doofBtn: document.getElementById("doofBtn"),
 }
 
-const firstHalf = products.slice(0, products.length / 2);
-const secondHalf = products.slice(products.length / 2);
+function filteringBool(productAttr) {
+  const productsFiltered = products.filter(product => product[productAttr] == true)
+  const firstHalf = productsFiltered.slice(0, Math.ceil(productsFiltered.length / 3));
+  const secondHalf = productsFiltered.slice(Math.ceil(productsFiltered.length / 3), 2*Math.ceil(productsFiltered.length / 3));
+  const thirdHalf = productsFiltered.slice(2*Math.ceil(productsFiltered.length / 3));
+  firstHalf.forEach(card => {
+    DOMSelectors.products1.insertAdjacentHTML("afterbegin",
+      `
+      <div id=card>
+      <img src=${card.image}>
+      <h1 id="text"> this is ${card.name} </h1>
+      </img>
+      
+      </div>
+      `
+    )
+  })
+  secondHalf.forEach(card => {
+    DOMSelectors.products2.insertAdjacentHTML("afterbegin",
+      `
+      <div id=card>
+      <img src=${card.image}>
+      <h1 id="text"> this is ${card.name} </h1>
+      </img>
+      
+      </div>
+      `
+    )
+  })
+  thirdHalf.forEach(card => {
+    DOMSelectors.products3.insertAdjacentHTML("afterbegin",
+      `
+      <div id=card>
+      <img src=${card.image}>
+      <h1 id="text"> this is ${card.name} </h1>
+      </img>
+      
+      </div>
+      `
+    )
+  })
+}
 
-firstHalf.forEach(card => {
-  DOMSelectors.products1.insertAdjacentHTML("afterbegin",
-    `
-    <div id=card>
-    <img src=${card.image}>
-    <h1 id="text"> this is ${card.name} </h1>
-    </img>
-    
-    </div>
-    `
-  )
-})
-secondHalf.forEach(card => {
-  DOMSelectors.products2.insertAdjacentHTML("afterbegin",
-    `
-    <div id=card>
-    <img src=${card.image}>
-    <h1 id="text"> this is ${card.name} </h1>
-    </img>
-    
-    </div>
-    `
-  )
-})
+function clearProducts () {
+  DOMSelectors.products1.innerHTML = ""
+  DOMSelectors.products2.innerHTML = ""
+  DOMSelectors.products3.innerHTML = ""
+}
 
+filteringBool("exists")
+
+DOMSelectors.doofBtn.addEventListener("click", function () {
+  clearProducts()
+  filteringBool("food")
+  })
 
 //theme buttons
 document.getElementById("themebtton").addEventListener("click", function () {
