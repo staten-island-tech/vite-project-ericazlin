70c54f5458d9d78d95097a0b6e8fd72925dcd5a0
ericazlin <ericazlin@gmail.com>
2022-12-31 15:39:18 -0500

    tidy up and stuffs

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,39 +2,29 @@ import '../styles/style.css'
 import {products} from './products.js'
 
 const DOMSelectors = {
-    products1: document.getElementById("products1"),
-    products2: document.getElementById("products2"),
-    products3: document.getElementById("products3"),
     everything: document.querySelector(".everything"),
-    everything2: document.querySelector(".everything2"),
     btn: document.querySelector(".btn"),
     filter: document.getElementById("filter"),
-    doofBtn: document.getElementById("doofBtn"),
-    existBtn: document.getElementById("existBtn"),
-    frozenBtn: document.getElementById("frozenBtn"),
-    snackBtn: document.getElementById("snackBtn"),
-    freshBtn: document.getElementById("freshBtn"),
-    chemicalsBtn: document.getElementById("chemicalsBtn"),
-    hotFoodBtn: document.getElementById("hotFoodBtn"),
-    carPartsBtn: document.getElementById("carPartsBtn"),
-    drinksBtn: document.getElementById("drinksBtn"),
-    coldFoodBtn: document.getElementById("coldFoodBtn"),
-    candyBtn: document.getElementById("candyBtn"),
-    bakedBtn: document.getElementById("bakedBtn"),
+        doofBtn: document.getElementById("doofBtn"),
+        existBtn: document.getElementById("existBtn"),
+        frozenBtn: document.getElementById("frozenBtn"),
+        snackBtn: document.getElementById("snackBtn"),
+        freshBtn: document.getElementById("freshBtn"),
+        chemicalsBtn: document.getElementById("chemicalsBtn"),
+        hotFoodBtn: document.getElementById("hotFoodBtn"),
+        carPartsBtn: document.getElementById("carPartsBtn"),
+        drinksBtn: document.getElementById("drinksBtn"),
+        coldFoodBtn: document.getElementById("coldFoodBtn"),
+        candyBtn: document.getElementById("candyBtn"),
+        bakedBtn: document.getElementById("bakedBtn"),
 }
 
 function filteringBool(productAttr) {
 
     const productsFiltered = products.filter(product => product[productAttr] == true)
 
-
-    const firstHalf = productsFiltered.slice(0, Math.ceil(productsFiltered.length / 3));
-    const secondHalf = productsFiltered.slice(Math.ceil(productsFiltered.length / 3), 2 * Math.ceil(productsFiltered.length / 3));
-    const thirdHalf = productsFiltered.slice(2 * Math.ceil(productsFiltered.length / 3));
-
-
-    firstHalf.forEach(card => {
-        DOMSelectors.products1.insertAdjacentHTML("afterbegin",
+    productsFiltered.forEach(card => {
+        DOMSelectors.everything.insertAdjacentHTML("afterbegin",
                                                   `
       <div id=card>
       <img src=${card.image}>
@@ -45,36 +35,11 @@ function filteringBool(productAttr) {
       `
       )
     })
-    secondHalf.forEach(card => {
-        DOMSelectors.products2.insertAdjacentHTML("afterbegin",
-                                                  `
-      <div id=card>
-      <img src=${card.image}>
-      <h1 id="text"> this is ${card.name} </h1>
-      </img>
-
-      </div>
-      `
-      )
-    })
-    thirdHalf.forEach(card => {
-        DOMSelectors.products3.insertAdjacentHTML("afterbegin",
-                                                  `
-      <div id=card>
-      <img src=${card.image}>
-      <h1 id="text"> this is ${card.name} </h1>
-      </img>
-
-      </div>
-      `
-      )
-    })
+    console.log(productsFiltered)
 }
 
 function clearProducts() {
-    DOMSelectors.products1.innerHTML = ""
-    DOMSelectors.products2.innerHTML = ""
-    DOMSelectors.products3.innerHTML = ""
+    DOMSelectors.everything.innerHTML = ""
 }
 
 filteringBool("exists")
@@ -141,13 +106,9 @@ DOMSelectors.bakedBtn.addEventListener("click", function () {
 
 //theme buttons
 document.getElementById("themebtton").addEventListener("click", function () {
-    if (document.body.classList.contains("pinkMode")) {
-        document.body.classList.add("greenMode");
-        document.body.classList.remove("pinkMode");
+    if (document.body.classList.contains("greenMode")) {
+        document.body.classList.replace("greenMode", 'pinkMode');
     } else {
-        document.body.classList.add("pinkMode");
-        document.body.classList.remove("greenMode");
+        document.body.classList.replace("pinkMode", 'greenMode');
     }
 });
-
-
